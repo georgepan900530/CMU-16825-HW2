3334dfae1d2037203cb1b0b29ce8cf5251f3c9e1
George  Fu-Cheng Pan <georgepan900530@gmail.com>
2025-02-07 11:43:09 -0500

    Add binary cross entropy loss

--- losses.py
+++ losses.py
@@ -1,20 +1,24 @@
 import torch
 
-# define losses
-def voxel_loss(voxel_src,voxel_tgt):
-	# voxel_src: b x h x w x d
-	# voxel_tgt: b x h x w x d
-	# loss = 
-	# implement some loss for binary voxel grids
-	return loss
 
-def chamfer_loss(point_cloud_src,point_cloud_tgt):
-	# point_cloud_src, point_cloud_src: b x n_points x 3  
-	# loss_chamfer = 
-	# implement chamfer loss from scratch
-	return loss_chamfer
+# define losses
+def voxel_loss(voxel_src, voxel_tgt):
+    # voxel_src: b x h x w x d
+    # voxel_tgt: b x h x w x d
+    # loss = Binary Cross Entropy
+    # implement some loss for binary voxel grids
+    loss = torch.nn.functional.binary_cross_entropy(voxel_src, voxel_tgt)
+    return loss
+
+
+def chamfer_loss(point_cloud_src, point_cloud_tgt):
+    # point_cloud_src, point_cloud_src: b x n_points x 3
+    # loss_chamfer =
+    # implement chamfer loss from scratch
+    return loss_chamfer
+
 
 def smoothness_loss(mesh_src):
-	# loss_laplacian = 
-	# implement laplacian smoothening loss
-	return loss_laplacian
+    # loss_laplacian =
+    # implement laplacian smoothening loss
+    return loss_laplacian
