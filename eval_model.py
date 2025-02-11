5d2146ed1d21911828b085dbe195ad2da8927f20
George  Fu-Cheng Pan <georgepan900530@gmail.com>
2025-02-10 23:50:05 -0500

    Add visualization

--- eval_model.py
+++ eval_model.py
@@ -3,71 +3,87 @@ import time
 import torch
 from model import SingleViewto3D
 from r2n2_custom import R2N2
-from  pytorch3d.datasets.r2n2.utils import collate_batched_R2N2
+from pytorch3d.datasets.r2n2.utils import collate_batched_R2N2
 import dataset_location
 import pytorch3d
 from pytorch3d.ops import sample_points_from_meshes
 from pytorch3d.ops import knn_points
 import mcubes
 import utils_vox
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
 from pytorch3d.transforms import Rotate, axis_angle_to_matrix
 import math
 import numpy as np
+from vis import *
+
 
 def get_args_parser():
-    parser = argparse.ArgumentParser('Singleto3D', add_help=False)
-    parser.add_argument('--arch', default='resnet18', type=str)
-    parser.add_argument('--vis_freq', default=1000, type=int)
-    parser.add_argument('--batch_size', default=1, type=int)
-    parser.add_argument('--num_workers', default=0, type=int)
-    parser.add_argument('--type', default='vox', choices=['vox', 'point', 'mesh'], type=str)
-    parser.add_argument('--n_points', default=1000, type=int)
-    parser.add_argument('--w_chamfer', default=1.0, type=float)
-    parser.add_argument('--w_smooth', default=0.1, type=float)  
-    parser.add_argument('--load_checkpoint', action='store_true')  
-    parser.add_argument('--device', default='cuda', type=str) 
-    parser.add_argument('--load_feat', action='store_true') 
+    parser = argparse.ArgumentParser("Singleto3D", add_help=False)
+    parser.add_argument("--arch", default="resnet18", type=str)
+    parser.add_argument("--vis_freq", default=1000, type=int)
+    parser.add_argument("--batch_size", default=1, type=int)
+    parser.add_argument("--num_workers", default=0, type=int)
+    parser.add_argument(
+        "--type", default="vox", choices=["vox", "point", "mesh"], type=str
+    )
+    parser.add_argument("--n_points", default=1000, type=int)
+    parser.add_argument("--w_chamfer", default=1.0, type=float)
+    parser.add_argument("--w_smooth", default=0.1, type=float)
+    parser.add_argument("--load_checkpoint", action="store_true")
+    parser.add_argument("--device", default="cuda", type=str)
+    parser.add_argument("--load_feat", action="store_true")
     return parser
 
+
 def preprocess(feed_dict, args):
-    for k in ['images']:
+    for k in ["images"]:
         feed_dict[k] = feed_dict[k].to(args.device)
 
-    images = feed_dict['images'].squeeze(1)
-    mesh = feed_dict['mesh']
+    images = feed_dict["images"].squeeze(1)
+    mesh = feed_dict["mesh"]
     if args.load_feat:
-        images = torch.stack(feed_dict['feats']).to(args.device)
+        images = torch.stack(feed_dict["feats"]).to(args.device)
 
     return images, mesh
+
 
 def save_plot(thresholds, avg_f1_score, args):
     fig = plt.figure()
     ax = fig.add_subplot(111)
-    ax.plot(thresholds, avg_f1_score, marker='o')
-    ax.set_xlabel('Threshold')
-    ax.set_ylabel('F1-score')
-    ax.set_title(f'Evaluation {args.type}')
-    plt.savefig(f'eval_{args.type}', bbox_inches='tight')
+    ax.plot(thresholds, avg_f1_score, marker="o")
+    ax.set_xlabel("Threshold")
+    ax.set_ylabel("F1-score")
+    ax.set_title(f"Evaluation {args.type}")
+    plt.savefig(f"eval_{args.type}", bbox_inches="tight")
 
 
 def compute_sampling_metrics(pred_points, gt_points, thresholds, eps=1e-8):
     metrics = {}
     lengths_pred = torch.full(
-        (pred_points.shape[0],), pred_points.shape[1], dtype=torch.int64, device=pred_points.device
+        (pred_points.shape[0],),
+        pred_points.shape[1],
+        dtype=torch.int64,
+        device=pred_points.device,
     )
     lengths_gt = torch.full(
-        (gt_points.shape[0],), gt_points.shape[1], dtype=torch.int64, device=gt_points.device
+        (gt_points.shape[0],),
+        gt_points.shape[1],
+        dtype=torch.int64,
+        device=gt_points.device,
     )
 
     # For each predicted point, find its neareast-neighbor GT point
-    knn_pred = knn_points(pred_points, gt_points, lengths1=lengths_pred, lengths2=lengths_gt, K=1)
+    knn_pred = knn_points(
+        pred_points, gt_points, lengths1=lengths_pred, lengths2=lengths_gt, K=1
+    )
     # Compute L1 and L2 distances between each pred point and its nearest GT
     pred_to_gt_dists2 = knn_pred.dists[..., 0]  # (N, S)
     pred_to_gt_dists = pred_to_gt_dists2.sqrt()  # (N, S)
 
     # For each GT point, find its nearest-neighbor predicted point
-    knn_gt = knn_points(gt_points, pred_points, lengths1=lengths_gt, lengths2=lengths_pred, K=1)
+    knn_gt = knn_points(
+        gt_points, pred_points, lengths1=lengths_gt, lengths2=lengths_pred, K=1
+    )
     # Compute L1 and L2 dists between each GT point and its nearest pred point
     gt_to_pred_dists2 = knn_gt.dists[..., 0]  # (N, S)
     gt_to_pred_dists = gt_to_pred_dists2.sqrt()  # (N, S)
@@ -85,11 +101,14 @@ def compute_sampling_metrics(pred_points, gt_points, thresholds, eps=1e-8):
     metrics = {k: v.cpu() for k, v in metrics.items()}
     return metrics
 
+
 def evaluate(predictions, mesh_gt, thresholds, args):
     if args.type == "vox":
         voxels_src = predictions
-        H,W,D = voxels_src.shape[2:]
-        vertices_src, faces_src = mcubes.marching_cubes(voxels_src.detach().cpu().squeeze().numpy(), isovalue=0.5)
+        H, W, D = voxels_src.shape[2:]
+        vertices_src, faces_src = mcubes.marching_cubes(
+            voxels_src.detach().cpu().squeeze().numpy(), isovalue=0.5
+        )
         vertices_src = torch.tensor(vertices_src).float()
         faces_src = torch.tensor(faces_src.astype(int))
         mesh_src = pytorch3d.structures.Meshes([vertices_src], [faces_src])
@@ -115,9 +134,15 @@ def evaluate(predictions, mesh_gt, thresholds, args):
     return metrics
 
 
-
 def evaluate_model(args):
-    r2n2_dataset = R2N2("test", dataset_location.SHAPENET_PATH, dataset_location.R2N2_PATH, dataset_location.SPLITS_PATH, return_voxels=True, return_feats=args.load_feat)
+    r2n2_dataset = R2N2(
+        "test",
+        dataset_location.SHAPENET_PATH,
+        dataset_location.R2N2_PATH,
+        dataset_location.SPLITS_PATH,
+        return_voxels=True,
+        return_feats=args.load_feat,
+    )
 
     loader = torch.utils.data.DataLoader(
         r2n2_dataset,
@@ -125,7 +150,8 @@ def evaluate_model(args):
         num_workers=args.num_workers,
         collate_fn=collate_batched_R2N2,
         pin_memory=True,
-        drop_last=True)
+        drop_last=True,
+    )
     eval_loader = iter(loader)
 
     model = SingleViewto3D(args)
@@ -143,10 +169,10 @@ def evaluate_model(args):
     avg_r_score = []
 
     if args.load_checkpoint:
-        checkpoint = torch.load(f'checkpoint_{args.type}.pth')
-        model.load_state_dict(checkpoint['model_state_dict'])
+        checkpoint = torch.load(f"checkpoint_{args.type}.pth")
+        model.load_state_dict(checkpoint["model_state_dict"])
         print(f"Succesfully loaded iter {start_iter}")
-    
+
     print("Starting evaluating !")
     max_iter = len(eval_loader)
     for step in range(start_iter, max_iter):
@@ -167,28 +193,60 @@ def evaluate_model(args):
         # TODO:
         # if (step % args.vis_freq) == 0:
         #     # visualization block
-        #     #  rend = 
+        #     #  rend =
         #     plt.imsave(f'vis/{step}_{args.type}.png', rend)
-      
+        if (step % args.vis_freq) == 0:
+            if args.type == "vox":
+                # f1_01 = metrics["F1@0.010000"].float()
+                # Need to visualize 3 examples, including their imput RGB images, predicted voxels, , ground truth voxels, and ground truth meshes
+                # Visualize RGB image
+                rend = images_gt[0, ..., :3].detach().cpu().numpy().clip(0, 1)
+                plt.imsave(f"results/q2_vox_rgb_{step}.png", rend)
+
+                # Visualize predicted voxels
+                voxels_pred = predictions[0]
+                vis_voxel(voxels_pred, f"results/q2_vox_pred_{step}.gif")
+
+                # Visualize ground truth voxels
+                voxels_gt = feed_dict["voxels"].to(args.device)
+                voxels_gt = voxels_gt[0]
+                vis_voxel(voxels_gt, f"results/q2_vox_gt_{step}.gif")
+
+                # Visualize ground truth mesh
+                mesh_gt = mesh_gt[0]
+                vis_mesh(mesh_gt, f"results/q2_mesh_gt_{step}.gif")
 
         total_time = time.time() - start_time
         iter_time = time.time() - iter_start_time
 
-        f1_05 = metrics['F1@0.050000']
+        f1_05 = metrics["F1@0.050000"]
         avg_f1_score_05.append(f1_05)
-        avg_p_score.append(torch.tensor([metrics["Precision@%f" % t] for t in thresholds]))
+        avg_p_score.append(
+            torch.tensor([metrics["Precision@%f" % t] for t in thresholds])
+        )
         avg_r_score.append(torch.tensor([metrics["Recall@%f" % t] for t in thresholds]))
         avg_f1_score.append(torch.tensor([metrics["F1@%f" % t] for t in thresholds]))
 
-        print("[%4d/%4d]; ttime: %.0f (%.2f, %.2f); F1@0.05: %.3f; Avg F1@0.05: %.3f" % (step, max_iter, total_time, read_time, iter_time, f1_05, torch.tensor(avg_f1_score_05).mean()))
-    
+        print(
+            "[%4d/%4d]; ttime: %.0f (%.2f, %.2f); F1@0.05: %.3f; Avg F1@0.05: %.3f"
+            % (
+                step,
+                max_iter,
+                total_time,
+                read_time,
+                iter_time,
+                f1_05,
+                torch.tensor(avg_f1_score_05).mean(),
+            )
+        )
 
     avg_f1_score = torch.stack(avg_f1_score).mean(0)
 
-    save_plot(thresholds, avg_f1_score,  args)
-    print('Done!')
+    save_plot(thresholds, avg_f1_score, args)
+    print("Done!")
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser('Singleto3D', parents=[get_args_parser()])
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser("Singleto3D", parents=[get_args_parser()])
     args = parser.parse_args()
     evaluate_model(args)
